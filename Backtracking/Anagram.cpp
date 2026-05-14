9fd44bf8f3bb16b581c1d439ee7336fd707dfa19
Amirali <amiralimokhtarian@gmail.com>
2026-05-14 19:45:34 +0330

    ADD final AngramSolver

--- Backtracking/Anagram.cpp
+++ Backtracking/Anagram.cpp
@@ -15,19 +15,8 @@
 
 using namespace std;
 
-bool goForward(const string& word, const map<string, bool>& dictionary);
-
-vector<string> findValidCombinations(const vector<char>& letters,
-const map<string, bool>& dictionary);
-
-void generateCombination(
-    const vector<char>& letters,
-    const map<string, bool> &dictionary,
-    vector<bool>& used,
-    string& current,
-    vector<string>& result
-);
-
+bool goForward(const string& word, const vector<char> &letters, vector<bool>& used, int index);
+vector<string> findValidCombinations(const vector<char>& letters, const string& word);
 void processQuery(map<string, bool>& dictionary);
 void loadDictionary(map<string, bool>& dictionary, int n);
 void runGame();
@@ -38,41 +27,34 @@ int main()
     return 0;
 }
 
-bool goForward(const string &word, const map<string, bool> &dictionary)
+bool goForward(const string &word, const vector<char> &letters, vector<bool>& used, int index)
 {
-    for(const auto &p : dictionary)
+    if(index == word.size())
+        return true;
+    
+    for(int i = 0 ; i < letters.size() ; i++)
     {
-        const string &dict_word = p.first;
-
-        if(dict_word.size() < word.size())
-            continue;
-
-        bool ok = true;
-        for(int i = 0; i < word.size(); i++)
+        if(!used[i] && letters[i] == word[index])
         {
-            if(dict_word[i] != word[i])
-            {
-                ok = false;
-                break;
-            }
+            used[i] = true;
+            if(goForward(word, letters, used, index+1))
+                return true;
+            used[i] = false;            
         }
-
-        if(ok)
-            return true;
     }
-
     return false;
 }
 
-
-vector<string> findValidCombinations(const vector<char> &letters, const map<string, bool> &dictionary)
+vector<string> findValidCombinations(const vector<char> &letters, const string& word)
 {
     vector<string> result;
     
     vector<bool> used(letters.size() , false);
-    string current = "";
 
-    generateCombination(letters, dictionary, used, current, result);
+    if(goForward(word, letters, used, 0))
+    {
+        result.push_back(word);
+    }
 
     sort(result.begin() , result.end());
     result.erase(unique(result.begin(), result.end()), result.end());
@@ -81,44 +63,19 @@ vector<string> findValidCombinations(const vector<char> &letters, const map<stri
 }
 
 
-void generateCombination(
-    const vector<char> &letters,
-    const map<string, bool> &dictionary, 
-    vector<bool> &used, 
-    string &current, 
-    vector<string>& result)
-{
-    if(!current.empty() && goForward(current , dictionary))
-        result.push_back(current);
 
-    for(int i = 0 ; i < letters.size() ; i++)
-    {
-        if(!used[i])
-        {
-            used[i] = true;
-            current.push_back(letters[i]);
-            generateCombination(letters, dictionary, used, current, result);
-
-            //Backtracking
-            current.pop_back();
-            used[i] = false;
-        }
-    }
-}
-
-
-void processQuery(map<string, bool> &dictionary)
+void processQuery(map<string, bool>& dictionary)
 {
     int q;
     cin >> q;
     cin.ignore();
-    vector<vector<string>> built_words;
-     built_words.resize(q);
-    
-    for(int i = 0 ; i < q ; i++)
+
+    vector<vector<string>> built_words(q);
+
+    for(int i = 0; i < q; i++)
     {
         string line;
-        getline(cin , line);
+        getline(cin, line);
 
         vector<char> letters;
 
@@ -127,17 +84,37 @@ void processQuery(map<string, bool> &dictionary)
             if(ch != ' ')
                 letters.push_back(ch);
         }
-        sort(letters.begin() , letters.end());
 
-        built_words[i] = findValidCombinations(letters , dictionary); 
+        sort(letters.begin(), letters.end());
+
+        for(auto& word : dictionary)
+        {
+            if(word.second)
+                continue;
+
+            vector<string> temp = findValidCombinations(letters, word.first);
+
+            for(const auto& w : temp)
+            {
+                built_words[i].push_back(w);
+                word.second = true;
+            }
+        }
+
+        for(auto& word : dictionary)
+        {
+            word.second = false;
+        }
     }
 
-    for(int i = 0 ; i < built_words.size() ; i++)
+    for(int i = 0; i < built_words.size(); i++)
     {
         cout << built_words[i].size() << endl;
-        
-        for(int j = 0 ; j < built_words[i].size() ; j++)
+
+        for(int j = 0; j < built_words[i].size(); j++)
+        {
             cout << built_words[i][j] << endl;
+        }
     }
 }
 
